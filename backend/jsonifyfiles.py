542f47423c65b2638795d0167ed0430b8df31b5f
druhib <bhargava.d@northeastern.edu>
2025-09-22 18:51:34 -0400

    cleaner data

--- backend/jsonifyfiles.py
+++ backend/jsonifyfiles.py
@@ -2,11 +2,11 @@ import pandas as pd
 import json
 from json import loads, dumps
 
-file = 'Copper_tonne_complete_British_Geological_Survey.csv'
+file = 'Phosphate_tonne_1913_2019_complete_british_geological_survey.csv'
 
 df = pd.read_csv(file)
 
-df.to_json('Copper_tonne_complete_British_Geological_Survey.json', orient='records', indent=4)
+df.to_json('Phosphate_tonne_1913_2019_complete_british_geological_survey.csv.json', orient='records', indent=4)
 
 
 
