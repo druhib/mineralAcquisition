2bb040d38ae4bea24f41f84c9cba5d9913874f5c
druhib <bhargava.d@northeastern.edu>
2025-09-22 18:51:08 -0400

    frontend dataconfig

--- frontend/src/components/dataConfig.tsx
+++ frontend/src/components/dataConfig.tsx
@@ -1,7 +1,13 @@
 import { data } from "react-router";
 import ASMdata from "../../public/data/ASM_Workers.json";
 import BAUXITEdata from "../../public/data/Bauxite_tonne_1913_2019_British_Geological_Survey.json";
-// import COBALTdata from "../../public/data/Cobalt_tonne_1913_2019_British_Geological_Survey.json";
+import COBALTdata from "../../public/data/Cobalt_tonne_1913_2019_British_Geological_Survey.json";
+import COPPERdata from "../../public/data/Copper_tonne_complete_British_Geological_Survey.json"
+
+import MANGANESEdata from "../../public/data/Manganese_tonne_1913_2019_Complete_british_geological_survey.json"
+import MONAZITEdata from "../../public/data/Monazite_tonne_1913_2019_complete_british_geological_survey.json"
+import NICKELdata from "../../public/data/Nickel_tonne_1913_2019_complete_british_geological_survey.json"
+import PHOSPHATEdata from "../../public/data/Phosphate_tonne_1913_2019_complete_british_geological_survey.json"
 
 export const dataConfig = {
 
@@ -13,12 +19,38 @@ export const dataConfig = {
   BAUXITE:{
     label: "Bauxite",
     data: BAUXITEdata,
-    checked: false
+    
+  },
+  COBALT:{
+    label: "Cobalt",
+    data: COBALTdata,
+    
+  },
+  COPPER:{
+    label: "Copper",
+    data: COPPERdata,
+    
+  },
+  MANGANESE:{
+    label: "Manganese",
+    data: MANGANESEdata,
+    
+  },
+  MONAZITE:{
+    label: "Monazite",
+    data: MONAZITEdata,
+    
+  },
+  
+  NICKEL:{
+    label: "Nickel",
+    data: NICKELdata,
+    
+  },
+  CPHSOPHATE:{
+    label: "Phosphate",
+    data: PHOSPHATEdata,
+    
   }
-  // COBALT:{
-  //   label: "Cobalt",
-  //   data: COBALTdata,
-  //   checked: false
-  // }
 
 };
