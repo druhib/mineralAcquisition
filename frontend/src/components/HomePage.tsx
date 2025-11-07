0b8c5a622219fd9562df97dbd57732c03c7da072
druhib <bhargava.d@northeastern.edu>
2025-11-06 20:33:34 -0500

    new images and removed footer

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -13,132 +13,128 @@ const HomePage = () => {
                                 <h1>Where Materials Relevant to Modern Society Come From</h1>
                                 <p>A Historical Global Overview of Mineral Acquisition and Exploitation.</p>
             
-                                {/* <span className="container" style = {{paddingTop: '2rem', paddingBottom: '6rem'}}>
-										<img src="images/introductioncomic.png" alt="" style ={{ display: 'flex',  alignItems: 'center', width: '100%', height: '100%'}}/>
-								</span> */}
                               
 							</header>
 							
 							<div className="tiles" style = {{ alignItems: 'center'}}>
 								<article className="style1">
 									<span className="image">
-										<img src="images/rockphotos/aluminum.png" alt="" />
+										<img src="images/rockphotos/Bauxite_siimsepp_AdobeStock_52934386.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© siimsepp / AdobeStock</p>
 									
 									<a href="elements.html">
-										<h2>Alumnium </h2>
+										<h2>Buaxite</h2>
 										<div className="content">
-											<p>Bauxite from France</p>
+											<p> (aluminum ore) </p>
 										</div>
 									</a>
 								</article>
 								<article className="style2">
 									<span className="image">
-										<img src="images/rockphotos/cobalt.png" alt="" />
+										<img src="images/rockphotos/Cobalt_Valerie Garner_AdobeStock_51615139.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© Valerie Garner / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Cobalt</h2>
 										<div className="content">
-											<p>Cobalite from Canada</p>
+											{/* <p>Cobalite from Canada</p> */}
 										</div>
 									</a>
 								</article>
 								<article className="style3">
 									<span className="image">
-										<img src="images/rockphotos/copper.png" alt="" />
+										<img src="images/rockphotos/Copper_vvoe_AdobeStock_102491386.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© vvoe / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Copper</h2>
 										<div className="content">
-											<p>Native Copper from United Kingdom </p>
+											{/* <p>Native Copper from United Kingdom </p> */}
 										</div>
 									</a>
 								</article>
 								<article className="style4">
 									<span className="image">
-										<img src="images/rockphotos/gold.png" alt="" />
+										<img src="images/rockphotos/Gold_Elzbieta Sekowska_AdobeStock_25400183.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© Elzbieta Sekowska / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Gold</h2>
 										<div className="content">
-											<p>Carbon Leader Gold Ore from South Africa</p>
+											{/* <p>Carbon Leader Gold Ore from South Africa</p> */}
 										</div>
 									</a>
 								</article>
 								<article className="style5">
 									<span className="image">
-										<img src="images/rockphotos/iron.png" alt="" />
+										<img src="images/rockphotos/Iron(hematite)_michal812_AdobeStock_25214049.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© michal812 / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Iron</h2>
 										<div className="content">
-											<p>Hermatite from Madagascar</p>
+											{/* <p>Hermatite from Madagascar</p> */}
 										</div>
 									</a>
 								</article>
 								<article className="style6">
 									<span className="image">
-										<img src="images/rockphotos/lithium.png" alt="" />
+										<img src="images/rockphotos/Lithium_Henri Koskinen_AdobeStock_257006124.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© Henri Koskinen / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Lithium</h2>
 										<div className="content">
-											<p>Spudumene from Madagascar</p>
+											{/* <p>Spudumene from Madagascar</p> */}
 										</div>
 									</a>
 								</article>
 								<article className="style2">
 									<span className="image">
-										<img src="images/rockphotos/manganese.png" alt="" />
+										<img src="images/rockphotos/Manganese_Okan_AdobeStock_522571038.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© Okan / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Manganese</h2>
 										<div className="content">
-											<p>Pyrolusite from Brazil</p>
-										</div>
-									</a>
-								</article>
-								<article className="style3">
-									<span className="image">
-										<img src="images/rockphotos/monazite2.png" alt="" />
-									</span>
-									<a href="generic.html">
-										<h2>Monazite</h2>
-										<div className="content">
-											<p>Monazite from Madagascar</p>
+											{/* <p>Pyrolusite from Brazil</p> */}
 										</div>
 									</a>
 								</article>
 								<article className="style1">
 									<span className="image">
-										<img src="images/rockphotos/nickel.png" alt="" />
+										<img src="images/rockphotos/Nickel_Agnieszka_AdobeStock_371622481.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© Agnieszka / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Nickel</h2>
 										<div className="content">
-											<p>Pentlandite from Canada</p>
+											{/* <p>Pentlandite from Canada</p> */}
 										</div>
 									</a>
 								</article>
 								<article className="style5">
 									<span className="image">
-										<img src="images/rockphotos/phosphorus.png" alt="" />
+										<img src="images/rockphotos/PhosphateRock_Apatite_Ekaterina_AdobeStock_760534276.png" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© Apatite_Ekaterina / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Phosphorous</h2>
 										<div className="content">
-											<p>Apatite from Madagascar</p>
+											{/* <p>Apatite from Madagascar</p> */}
 										</div>
 									</a>
 								</article>
 								<article className="style6">
 									<span className="image">
-										<img src="images/rockphotos/silver.png" alt="" />
+										<img src="images/rockphotos/Silver_bambambu_AdobeStock_189623313.jpeg" alt="" />
 									</span>
+									<p style = {{fontSize:"x-small", fontWeight:"bold", marginTop:".75rem"}}>© bambambu / AdobeStock</p>
 									<a href="generic.html">
 										<h2>Silver</h2>
 										<div className="content">
-											<p>Native Silver from Czech Republic</p>
+											{/* <p>Native Silver from Czech Republic</p> */}
 										</div>
 									</a>
 								</article>
@@ -147,32 +143,7 @@ const HomePage = () => {
 					
 						</div>
 					</div>
-
-				{/* <!-- Footer --> */}
-					<footer id="footer">
-						<div className="inner">
-							<section>
-								<h2>Get in touch</h2>
-								<form method="post" action="#">
-									<div className="fields">
-										<div className="field half">
-											<input type="text" name="name" id="name" placeholder="Name" />
-										</div>
-										<div className="field half">
-											<input type="email" name="email" id="email" placeholder="Email" />
-										</div>
-										<div className="field">
-											<textarea name="message" id="message" placeholder="Message"></textarea>
-										</div>
-									</div>
-									<ul className="actions">
-										<li><input type="submit" value="Send" className="primary" /></li>
-									</ul>
-								</form>
-							</section>
-							
-						</div>
-					</footer>
+					
 
 			</div>
 
