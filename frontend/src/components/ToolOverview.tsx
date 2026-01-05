23660c5db935dde46334c3a2e30a372055c87b15
druhib <bhargava.d@northeastern.edu>
2026-01-05 10:11:35 +0000

    reviewed toolOverviewpage

--- frontend/src/components/ToolOverview.tsx
+++ frontend/src/components/ToolOverview.tsx
@@ -2,7 +2,6 @@ const ToolOverview= () => {
     return (
 
         <div id="wrapper" >
-        
             {/* <!-- Main --> */}
             <div id="main" style={{ paddingBottom: "1rem" }}>
                 <div className="inner" >
@@ -15,10 +14,10 @@ const ToolOverview= () => {
                                 There are three major sections of this website: </p>
                             <p><strong>Home Page</strong></p> 
                             
-                                <p>On the home page, you will find an interactive, color coded map that shows which countries have exported a given material over a given time period.
+                                <p>On the home page, you will find an interactive, color coded map that shows which countries have exported materials over a time period.
                                 The production data that is visualized in this map was transcribed from mineral commodity surveys published from the two oldest geological surveys in the world,
-                                the British Geological Survey (1913-2019), and the United States Geological Survey (1495-1900). Each one of these mineral commodity surveys report on the production of 40 different items.
-                                Due to time constraints, the authors constrained the scope of this work to 10 of the most important and widely used materials of the modern age. </p>
+                                the British Geological Survey (1493-1900), and the United States Geological Survey (1913-2019), with the exception of Nickel reported from 1913-2023. Each one of these mineral commodity surveys report on the production of 40 different items.
+                                Due to time constraints, the authors constrained the scope of this work to 10 widely used materials of the modern age. </p>
                             
                             <p><strong>Case Studies</strong></p>
                             <p> If you scroll past the interactive map, you will find a clickable photo of the materials included in this work. 
@@ -28,16 +27,16 @@ const ToolOverview= () => {
                                 The authors have geotagged some of the largest mining operations for the respective mineral. There are however so many more mines than the ones that 
                                 have been highlighted. </p>
                             
-                            <p><strong>What is mining?</strong></p>
+                            <p><strong>'What is Mining?' Page</strong></p>
                             <p> The authors have also made a page titled 'What is mining?' to give an overview of the different ways materials can be 
                                 extracted from the Earth's surface. 
                             </p>
                             
                             <h1> What is the motivation behind this work? </h1>
-                            <p>This work is inspired by Uruguayan economist Eduardo Galeano’s book Open Veins of Latin America which was published in 1971. 
+                            <p>This work is inspired by Uruguayan economist Eduardo Galeano's book Open Veins of Latin America which was published in 1971. 
                             Galeano examines how the extraction of materials and their trade relationships have impacted the people of South America. 
                             His work has made clear that as we pursue a sustainable, safe, and equitable global society, we must reconcile with the fact that Earth's resources
-                            are unevenly distributed. As engineers, scientists, and other industrial professionals design and implement exciting technologies 
+                            are unevenly distributed. As engineers, scientists, and other industrial professionals design and implement exciting technologies, 
                             they should also have to consider the consequences of how the materials required for these projects are acquired. 
                             
 
